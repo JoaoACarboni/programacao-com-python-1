1793ed25839dabe509086b67e4cbccdc0e1e0552
JoaoACarboni <joaocarbonigomes00@gmail.com>
2026-09-09 09:03:12 -0300

    Adicionando aula 21

---
+++ (21) FUNÇÕES/main.py
@@ -0,0 +1,13 @@
+print()
+int()
+# Funções são blocos de código que podem ser reutilizados várias vezes. Elas podem receber parâmetros e retornar valores. Em Python, as funções são definidas usando a palavra-chave `def`.
+def calcular_media():
+    print("Calculando a média...")
+# Nome - parâmetro
+def saudacao(nome):
+    print(f"Bem vindo! {nome}")
+
+calcular_media()
+saudacao("Cristhian") # Cristhian - argumento
+saudacao("Jonatan")
+saudacao("Fefe")
